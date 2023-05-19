38fab572be3ca2ecf98e9113dd0d71cded8723ee
Timur Bondarenko <tibode495@gmail.com>
2023-05-19 10:56:52 +0600

    setup vetur and ts

---
+++ vetur.config.js
@@ -0,0 +1,15 @@
+// vetur.config.js
+/** @type {import('vls').VeturConfig} */
+module.exports = {
+  settings: {
+    "vetur.useWorkspaceDependencies": false,
+    "vetur.experimental.templateInterpolationService": true,
+  },
+  projects: [
+    {
+      root: "./frontend",
+      package: "./package.json",
+      tsconfig: "./tsconfig.json",
+    },
+  ],
+};

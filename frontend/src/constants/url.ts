75ed224b76160883ed2f9d2879d99152473ff458
Emiliia <emiliatoropova@yandex.ru>
2023-05-20 22:01:32 +0300

    st 39: add absolute url to image src in cards

--- frontend/src/constants/url.ts
+++ frontend/src/constants/url.ts
@@ -1,3 +1,5 @@
 const isDevelopmnet = import.meta.env.MODE === 'development'
 
 export const API_URL = isDevelopmnet ? 'http://localhost:7080/api' : '/api'
+
+export const BASE_URL = isDevelopmnet ? 'http://localhost:7080/' : '/'

6479e286467a9903de36e419978e3ea72e01ac60
Timur Bondarenko <tibode495@gmail.com>
2023-05-20 09:27:20 +0600

    add deleting file

--- frontend/src/views/EventView.vue
+++ frontend/src/views/EventView.vue
@@ -1,22 +1,38 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { type EventOnPoster } from '@common/types/event'
-import { getEvent } from "@/services/events.services";
-import { useRoute } from 'vue-router'
+import { deleteEvent, getEvent } from '@/services/events.services'
+import { useRoute, useRouter } from 'vue-router'
 
-const posterEvent = ref<EventOnPoster>({})
-const route = useRoute();
+const posterEvent = ref<EventOnPoster | null>(null)
+const route = useRoute()
+const id = route.params.id
+const router = useRouter()
+
+if (!(typeof id === 'string' || id instanceof String)) {
+  router.push({ path: '/' })
+}
+
 const loadPosterEvent = async () => {
-  posterEvent.value = await getEvent(route.params.id)
+  posterEvent.value = await getEvent(id)
 }
 
 loadPosterEvent()
+
+const deleteCard = async () => {
+  await deleteEvent(id)
+  router.push({ path: '/' })
+}
 </script>
 
 <template>
-  <main>
-      <h2 class="title">{{ posterEvent.title }}</h2>
-     <pre>{{ posterEvent }}</pre>
+  <main v-if="posterEvent">
+    <h2 class="title">
+      {{ posterEvent.title }}
+
+      <button class="delete is-small" @click="deleteCard()"></button>
+    </h2>
+    <pre>{{ posterEvent }}</pre>
   </main>
 </template>
 

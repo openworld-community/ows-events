50a0bc66b719b54b715065d658404a83f219acaa
Emiliia <emiliatoropova@yandex.ru>
2023-05-23 20:13:37 +0300

    st 67: plug common styles

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import 'vue-final-modal/style.css'
+import '@/assets/less/app.less'
 import CommonHeader from '@/components/header/CommonHeader.vue'
 </script>
 

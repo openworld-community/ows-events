97e6492ee81fdf9e2614d47c976d7ac41822d84e
Miss_terry <Etein@yandex.ru>
2023-05-26 17:48:46 +0300

    Added design for About us page

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useTranslation } from '@/i18n'
+import aboutUsImg from '../../public/img/about-us@2x.jpg'
 
 const { t } = useTranslation()
 </script>
@@ -7,6 +8,7 @@ const { t } = useTranslation()
 <template>
   <section class="about">
     <h2 class="title">{{ t('about.title') }}</h2>
+    <img :src="aboutUsImg" class="about__img" alt="About Peredelano Afisha"/>
     <p>{{ t('about.p1') }}</p>
     <br />
     <p>{{ t('about.p2') }}</p>
@@ -15,7 +17,7 @@ const { t } = useTranslation()
     <br />
     <p>
       {{ t('about.github') }}:
-      <a href="https://github.com/openworld-community/ows-events">Repo</a>
+      <a href="https://github.com/openworld-community/ows-events" class="about__link">Repo</a>
     </p>
   </section>
 </template>
@@ -24,5 +26,34 @@ const { t } = useTranslation()
   .about {
     padding-left: var(--padding-side);
     padding-right: var(--padding-side);
+    padding-top: var(--padding-vertical);
+    padding-bottom: var(--padding-vertical);
+  }
+
+  .about p {
+    font-size: var(--font-size-S);
+    line-height: 20px;
+  }
+
+  .title {
+    font-size: var(--font-size-XXL);
+    line-height: var(--line-height-XXL);
+    font-weight: var(--font-weight-bold);
+  }
+
+  .about__img {
+    width: 100%;
+    min-width: 100%;
+    max-width: 100%;
+    height: 100%;
+    margin-top: var(--space-unrelated-items);
+    margin-bottom: var(--space-related-items);
+    border-radius: 4px;
+  }
+
+  .about__link {
+    color: var(--color-link);
+    text-decoration: underline;
+    font-weight: var(--font-weight-bold);
   }
 </style>

84fa6647be828e8d6326449454cd1add30c97fa6
Huge Letters <ckuka71@gmail.com>
2023-05-27 16:47:43 +0200

    DRAFT: nothing major

--- frontend-nuxt/app.vue
+++ frontend-nuxt/app.vue
@@ -1,4 +1,8 @@
+<script setup lang="ts">
+const { data } = await useFetch<{ title: string }>('https://jsonplaceholder.typicode.com/posts/1')
+</script>
 <template>
+  <span>{{ !data ? 'loading' : data.title }}</span>
   <div>
     <NuxtWelcome />
   </div>
--- frontend-nuxt/nuxt.config.ts
+++ frontend-nuxt/nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  modules: ['@pinia/nuxt'],
+  // на винде очень долго стартует дев-сервер, проблема недавняя
+  // один из авторов Нукста на ГХ посоветовал такое решение
+  // если у кого-то от этой настройки наоборот что-то ломается, то скажите - что-нибудь придумаем
+  experimental: { watcher: 'chokidar' }
 })

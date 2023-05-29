7e2f9c6d08f4a55e11498c5e62b367ed11d0a0d9
Emiliia <emiliatoropova@yandex.ru>
2023-05-29 17:29:22 +0300

    St 181: replace page

--- frontend/pages/about.vue
+++ frontend/pages/about.vue
@@ -1,19 +1,32 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import aboutUsImg from '../public/img/about-us@2x.jpg';
+import { REPO_URL } from '@/constants/url';
+</script>
 
 <template>
   <section class="about">
-    <h2 class="title">
-      {{ $translate('about.title') }}
-    </h2>
-    <p>{{ $translate('about.p1') }}</p>
+    <h2 class="title">{{ $translate('about.title') }}</h2>
+    <img
+      :src="aboutUsImg"
+      class="about__img"
+      :alt="$translate('about.alt')"
+    />
+    <p>{{ $translate('about.idea') }}</p>
     <br />
-    <p>{{ $translate('about.p2') }}</p>
+    <p>{{ $translate('about.functionality') }}</p>
     <br />
-    <p>{{ $translate('about.p3') }}</p>
+    <p>{{ $translate('about.monetization') }}</p>
     <br />
     <p>
       {{ $translate('about.github') }}:
-      <a href="https://github.com/openworld-community/ows-events">Repo</a>
+      <a
+        :href="REPO_URL"
+        class="about__link"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+        Repo
+      </a>
     </p>
   </section>
 </template>
@@ -22,5 +35,34 @@
 .about {
   padding-left: var(--padding-side);
   padding-right: var(--padding-side);
+  padding-top: var(--padding-vertical);
+  padding-bottom: var(--padding-vertical);
+}
+
+.about p {
+  font-size: var(--font-size-S);
+  line-height: 20px;
+}
+
+.title {
+  font-size: var(--font-size-XXL);
+  line-height: var(--line-height-XXL);
+  font-weight: var(--font-weight-bold);
+}
+
+.about__img {
+  width: 100%;
+  min-width: 100%;
+  max-width: 100%;
+  height: 100%;
+  margin-top: var(--space-unrelated-items);
+  margin-bottom: var(--space-related-items);
+  border-radius: 4px;
+}
+
+.about__link {
+  color: var(--color-link);
+  text-decoration: underline;
+  font-weight: var(--font-weight-bold);
 }
 </style>

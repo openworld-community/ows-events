e8daaa070cc229ec3f1454adf2876fea8baff1d2
Emiliia <emiliatoropova@yandex.ru>
2023-06-07 13:07:40 +0300

    St 275: fix redirecting bug

--- frontend/components/header/Authorisation.vue
+++ frontend/components/header/Authorisation.vue
@@ -3,23 +3,11 @@ import { UserInfo } from '../../../common/types/user';
 
 import { useModal } from 'vue-final-modal';
 import { ModalAuthorisation } from '#components';
-import { v4 } from 'uuid';
-import { AUTH_SERVER_URL, SERVER_URL } from '~/constants/url';
 
 const tokenCookie = useCookie<string | null>('token');
 
 const isAuthorized = computed(() => !!tokenCookie.value);
 const user = useCookie<UserInfo | null>('user');
-
-const authorize = () => {
-	const temporaryId = v4();
-	const authLink: string = `${AUTH_SERVER_URL}/auth/${temporaryId}?encodede_backurl=${encodeURIComponent(
-		`${SERVER_URL}/postauth/${temporaryId}`
-	)}`;
-	window.open(authLink, '_blank');
-	setTimeout(() => close(), 300);
-	updateModalData();
-};
 
 const deauthorize = () => {
 	user.value = null;
@@ -32,7 +20,6 @@ const updateModalData = () => {
 	patchOptions({
 		attrs: {
 			close: close,
-			authorize: authorize,
 			deauthorize: deauthorize,
 			isAuthorized: isAuthorized.value
 		}

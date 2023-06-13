9af931d89b9d9e999faa84407122445bc545f7d7
andrew <andrew91@mail.ru>
2023-06-13 16:10:58 +0300

    add schem image

--- backend/src/rest/v1/image/router.ts
+++ backend/src/rest/v1/image/router.ts
@@ -1,9 +1,10 @@
 import { FastifyInstance } from "fastify";
 import { addImage, deleteImage } from "./controller";
 import { IAddImageRouteProps, IDeleteImageRouteProps } from "./types";
+import { addImageSchema, deleteImageSchema } from "./schema";
 
 export const imageApi = async (fastify: FastifyInstance) => {
-  fastify.post<IDeleteImageRouteProps>("/delete", deleteImage);
+  fastify.post<IDeleteImageRouteProps>("/delete", { schema: deleteImageSchema, handler: deleteImage } );
 
-  fastify.post<IAddImageRouteProps>("/add", addImage);
+  fastify.post<IAddImageRouteProps>("/add", { schema: addImageSchema, handler: addImage });
 };

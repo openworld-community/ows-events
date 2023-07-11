81c9be57f0cb83580d588c65d91578b4ac892916
igor.la <igor.la@fora-soft.com>
2023-07-11 16:30:25 +0300

    Fix token ttl (add maxAge - 30 days)

--- frontend/pages/postauth/[id].vue
+++ frontend/pages/postauth/[id].vue
@@ -5,13 +5,13 @@ definePageMeta({
 	middleware: async () => {
 		const route = useRoute();
 		const userToken = getFirstParam(route.params.id);
-		useCookie<string>('token').value = userToken;
+		useCookie<string>('token', { maxAge: 60 * 60 * 24 * 30 }).value = userToken;
 		const { data: user } = await apiRouter.auth.getUser.useQuery({ data: { userToken } });
 		if (!user.value) {
 			console.error('No user data retrieved');
 			return;
 		}
-		useCookie<UserInfo | null>('user').value = user.value;
+		useCookie<UserInfo | null>('user', { maxAge: 60 * 60 * 24 * 30 }).value = user.value;
 	}
 });
 await navigateTo({ name: RouteNameEnum.HOME });

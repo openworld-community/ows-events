2dfff3b32f16157f3faa0092ee19ecf438cb8827
Dichell <9792910@gmail.com>
2023-07-11 23:41:35 +0300

    onMounted

--- frontend/components/home/UserLocation.vue
+++ frontend/components/home/UserLocation.vue
@@ -5,64 +5,66 @@ import type { UserLocation } from '../../../common/types/location';
 
 const locationStore = useLocationStore();
 
-navigator.geolocation.getCurrentPosition(
+onMounted(()=>{
+    navigator.geolocation.getCurrentPosition(
 
-// if user allowed to get GEO, do this:
-    async position => {
-            try {
+    // if user allowed to get GEO, do this:
+        async position => {
+                try {
+                    const { data } = await useFetch(
+                        "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude="+ 
+                        position.coords.latitude +
+                        "&longitude=" +
+                        position.coords.longitude,
+                        {
+                            lazy: true,
+                            server: false,
+                            transform: (data: {
+                                city: string; countryCode: string; countryName: string
+                            }): UserLocation => {
+                                return {
+                                    code: data.countryCode,
+                                    city: data.city,
+                                    country: data.countryName
+                                };
+                            }
+                        }
+                    )
+                    if (!data.value) return;
+                    locationStore.userLocation = data.value
+                } catch (error: any) {
+                    console.log("error: ", error.message);
+                } 
+        },
+
+    // if user not allowed to get his GEO
+        async error => {
+            if(error.message == 'User denied Geolocation'){
                 const { data } = await useFetch(
-                    // free api, 30 request per day
-                    "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude="+ 
-                    position.coords.latitude +
-                    "&longitude=" +
-                    position.coords.longitude,
+                    process.server ? useRequestHeaders()['x-forwarded-for'] : LOCATION_API_URL,
                     {
-                        lazy: true,
-                        server: false,
+                        query: {
+                            key: import.meta.env.VITE_IPREGISTRY_API_KEY ?? process.env.VITE_IPREGISTRY_API_KEY
+                        },
+                        baseURL: LOCATION_API_URL,
                         transform: (data: {
-                            city: string; countryCode: string; countryName: string
+                            location: { city: string; country: { code: string; name: string } };
                         }): UserLocation => {
                             return {
-                                code: data.countryCode,
-                                city: data.city,
-                                country: data.countryName
+                                code: data.location.country.code,
+                                city: data.location.city,
+                                country: data.location.country.name
                             };
                         }
                     }
-                )
+                );
                 if (!data.value) return;
                 locationStore.userLocation = data.value
-            } catch (error: any) {
-                console.log("error: ", error.message);
-            } 
-    },
-
-// if user not allowed to get his GEO
-    async error => {
-        if(error.message == 'User denied Geolocation'){
-            const { data } = await useFetch(
-                process.server ? useRequestHeaders()['x-forwarded-for'] : LOCATION_API_URL,
-                {
-                    query: {
-                        key: import.meta.env.VITE_IPREGISTRY_API_KEY ?? process.env.VITE_IPREGISTRY_API_KEY
-                    },
-                    baseURL: LOCATION_API_URL,
-                    transform: (data: {
-                        location: { city: string; country: { code: string; name: string } };
-                    }): UserLocation => {
-                        return {
-                            code: data.location.country.code,
-                            city: data.location.city,
-                            country: data.location.country.name
-                        };
-                    }
-                }
-            );
-            if (!data.value) return;
-            locationStore.userLocation = data.value
+            }
         }
-    }
-)
+    )
+})
+
 
 </script>
 

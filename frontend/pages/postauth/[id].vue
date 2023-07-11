3c73b8eb4893064e0df46dffc98f70af9b81450f
igor.la <igor.la@fora-soft.com>
2023-07-11 18:22:44 +0300

    Fix after review

--- frontend/pages/postauth/[id].vue
+++ frontend/pages/postauth/[id].vue
@@ -1,17 +1,18 @@
 <script setup lang="ts">
 import { RouteNameEnum } from '~/constants/enums/route';
 import type { UserInfo } from '../../../common/types/user';
+import { TOKEN_MAX_AGE_SECONDS } from '../../constants/defaultValues/time';
 definePageMeta({
 	middleware: async () => {
 		const route = useRoute();
 		const userToken = getFirstParam(route.params.id);
-		useCookie<string>('token', { maxAge: 60 * 60 * 24 * 30 }).value = userToken;
+		useCookie<string>('token', { maxAge: TOKEN_MAX_AGE_SECONDS }).value = userToken;
 		const { data: user } = await apiRouter.auth.getUser.useQuery({ data: { userToken } });
 		if (!user.value) {
 			console.error('No user data retrieved');
 			return;
 		}
-		useCookie<UserInfo | null>('user', { maxAge: 60 * 60 * 24 * 30 }).value = user.value;
+		useCookie<UserInfo | null>('user', { maxAge: TOKEN_MAX_AGE_SECONDS }).value = user.value;
 	}
 });
 await navigateTo({ name: RouteNameEnum.HOME });

4f8de6e55a8da5f5e0185fd0bdd6499cc0af53a2
Dichell <9792910@gmail.com>
2023-07-28 22:33:15 +0300

    migration passed successful

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -20,6 +20,7 @@ import { tagsApi } from './rest/v1/tags/router';
 import { migrate } from './migrations/price-structure-18-07-23';
 import { userApi } from './rest/v1/user/router';
 import { migrate as migrateUserStructure } from './migrations/user-structure-12-06-23';
+import { migrate as migrateEventsStructure } from './migrations/events-structure-25-07-23';
 
 
 const server = fastify({
@@ -34,6 +35,7 @@ connectToMongo()
 		// TODO: Remove after merge ASAP!
 		migrate().then();
 		migrateUserStructure();
+        migrateEventsStructure();
 		// eslint-disable-next-line no-console
 		console.log('Connected to mongo');
 	})

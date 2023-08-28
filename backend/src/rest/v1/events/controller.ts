31a343a590df98caf5afbe584047f3f1f8b437a6
igor.la <igor.la@fora-soft.com>
2023-08-28 18:27:13 +0300

    Hotfix import

--- backend/src/rest/v1/events/controller.ts
+++ backend/src/rest/v1/events/controller.ts
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
 import { EventOnPoster } from '@common/types';
-import { EventTypes } from '@common/const/eventTypes';
+import { EventTypes } from '../../../../../common/const/eventTypes';
 import { CommonErrorsEnum } from '../../../../../common/const';
 import { eventsStateController } from '../../../controllers/events-state-controller';
 import {
@@ -31,11 +31,11 @@ export const addEvent: IAddEventHandler = async (request) => {
 		event.creatorId = 'dev-user';
 	}
 
-    if (event.creatorId === 'parser'){
-        event.type = EventTypes.PARSED
-    } else {
-        event.type = EventTypes.USER_GENERATED
-    }
+	if (event.creatorId === 'parser') {
+		event.type = EventTypes.PARSED;
+	} else {
+		event.type = EventTypes.USER_GENERATED;
+	}
 
 	const eventWithThisLink = await EventModel.findOne({
 		url: event.url,

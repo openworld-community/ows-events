997c631b78ee270406da61cc4c120db43afac9c1
Emiliia <emiliatoropova@yandex.ru>
2023-09-06 17:16:35 +0300

    fix types

--- frontend/utils/dates.ts
+++ frontend/utils/dates.ts
@@ -4,29 +4,32 @@ import utc from 'dayjs/plugin/utc';
 dayjs.extend(utc);
 
 export type Time = { hours: number | string; minutes: number | string; seconds?: number | string };
-type LocaleObject = {
-	code: string;
-	name: string;
-	iso: string;
-	dir: string;
-	files: string[];
-	hashes: string[];
-	types: string[];
-};
+// type LocaleObject = {
+// 	code: string;
+// 	name: string;
+// 	iso: string;
+// 	dir: string;
+// 	files: string[];
+// 	hashes: string[];
+// 	types: string[];
+// };
 
 export const getDateFromEpochInMs = (epoch: number | undefined, keepTimezone = false) => {
 	if (!epoch) return null;
 	const djs = !keepTimezone ? dayjs.utc(epoch) : dayjs(epoch);
 	if (!djs.isValid()) return null;
 	return djs.toDate();
-}
+};
 
-export const getTimeFromEpochInMs= (epoch: number | undefined, keepTimezone = false): Time | null => {
+export const getTimeFromEpochInMs = (
+	epoch: number | undefined,
+	keepTimezone = false
+): Time | null => {
 	if (!epoch) return null;
 	const djs = !keepTimezone ? dayjs.utc(epoch) : dayjs(epoch);
 	if (!djs.isValid()) return null;
 	return { hours: djs.hour(), minutes: djs.minute(), seconds: djs.second() };
-}
+};
 
 /**
  *
@@ -43,21 +46,19 @@ export const combineDateTime = (date: Date | null, time: Time | null): Date => {
 		.set('minute', +time.minutes)
 		.set('second', +(time.seconds ?? 0))
 		.toDate();
-}
+};
 
 export const convertToLocaleString = (epoch: number) => {
-	const { locale, locales } = useI18n();
-	const locIndex = locales.value.findIndex(
-		(el: LocaleObject): boolean => el.code === locale.value
-	);
-	return new Date(epoch).toLocaleString(locales.value[locIndex].iso, {
+	const { locale, locales} = useI18n();
+	const locIndex = locales.value.findIndex((el: typeof locales.value[number]) => el.code as string === locale.value);
+	return new Date(epoch).toLocaleString(locales.value[locIndex].iso as string, {
 		timeZone: 'UTC',
 		month: 'long',
 		day: 'numeric',
 		hour: '2-digit',
 		minute: '2-digit'
 	});
-}
+};
 
 export const day = 1000 * 60 * 60 * 24;
 // Second date getTime() must be greater than firstDate

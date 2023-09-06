09d1e4fd8c581724d460c92b9d2ca33ef734977e
Emiliia <emiliatoropova@yandex.ru>
2023-09-06 16:47:52 +0300

    fix types

--- frontend/utils/dates.ts
+++ frontend/utils/dates.ts
@@ -1,27 +1,40 @@
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
+
 dayjs.extend(utc);
 
 export type Time = { hours: number | string; minutes: number | string; seconds?: number | string };
-export function getDateFromEpochInMs(epoch: number | undefined, keepTimezone = false) {
+type LocaleObject = {
+	code: string;
+	name: string;
+	iso: string;
+	dir: string;
+	files: string[];
+	hashes: string[];
+	types: string[];
+};
+
+export const getDateFromEpochInMs = (epoch: number | undefined, keepTimezone = false) => {
 	if (!epoch) return null;
 	const djs = !keepTimezone ? dayjs.utc(epoch) : dayjs(epoch);
 	if (!djs.isValid()) return null;
 	return djs.toDate();
 }
-export function getTimeFromEpochInMs(epoch: number | undefined, keepTimezone = false): Time | null {
+
+export const getTimeFromEpochInMs= (epoch: number | undefined, keepTimezone = false): Time | null => {
 	if (!epoch) return null;
 	const djs = !keepTimezone ? dayjs.utc(epoch) : dayjs(epoch);
 	if (!djs.isValid()) return null;
 	return { hours: djs.hour(), minutes: djs.minute(), seconds: djs.second() };
 }
+
 /**
  *
  * @param date will be interpreted as UTC Date object. If not provided will use current date
  * @param time time to combine with the provided date. If not provided will be set to midnight
  * @returns a UTC Date object
  */
-export function combineDateTime(date: Date | null, time: Time | null): Date {
+export const combineDateTime = (date: Date | null, time: Time | null): Date => {
 	date ??= new Date();
 	time ??= { hours: 0, minutes: 0, seconds: 0 };
 	return dayjs
@@ -32,13 +45,12 @@ export function combineDateTime(date: Date | null, time: Time | null): Date {
 		.toDate();
 }
 
-export function convertToLocaleString(epoch: number) {
-	const {locale, locales} = useI18n()
-	const locIndex = locales.value.findIndex((el: any) => el.code === locale.value)
-
-	console.log(locales.value)
-
-	return new Date(epoch).toLocaleString(locales.value[locIndex].iso as string, {
+export const convertToLocaleString = (epoch: number) => {
+	const { locale, locales } = useI18n();
+	const locIndex = locales.value.findIndex(
+		(el: LocaleObject): boolean => el.code === locale.value
+	);
+	return new Date(epoch).toLocaleString(locales.value[locIndex].iso, {
 		timeZone: 'UTC',
 		month: 'long',
 		day: 'numeric',

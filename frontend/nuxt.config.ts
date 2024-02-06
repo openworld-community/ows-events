16515afb703040e85d7dcb87fefda7008ca6fb48
igor.la <igor.la@fora-soft.com>
2024-02-06 20:17:52 +0300

    Fix nuxt cors reedirect (probably) for local auth

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,6 +3,7 @@ import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 import path from 'path';
 import { fileURLToPath, URL } from 'node:url';
 import { searchForWorkspaceRoot } from 'vite';
+import { BASE_URL } from './constants/url';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
@@ -29,7 +30,14 @@ export default defineNuxtConfig({
 	],
 	routeRules: {
 		'/': { redirect: '/ru', ssr: true },
-		'/**': { cors: false }
+		'/**': {
+			cors: true,
+			headers: {
+				'Access-Control-Allow-Origin': BASE_URL,
+				'Access-Control-Allow-Methods': 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
+				'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Requested-With'
+			}
+		}
 	},
 	i18n: {
 		debug: false,

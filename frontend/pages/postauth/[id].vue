37efd0c1888e2c69753ceb90a3dfd5446c5d9e66
igor.la <igor.la@fora-soft.com>
2024-02-10 17:50:37 +0300

    Fix bug with no user id saved on frontend

--- frontend/pages/postauth/[id].vue
+++ frontend/pages/postauth/[id].vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { RoutePathEnum } from '~/constants/enums/route';
 import { TOKEN_MAX_AGE_SECONDS } from '../../constants/defaultValues/time';
-import type { UserInfo } from '../../../common/types/user';
+import type { UserInfoWithId } from '../../../common/types/user';
 import { CookieNameEnum } from '../../constants/enums/common';
 
 defineI18nRoute(false);
@@ -13,12 +13,11 @@ definePageMeta({
 		useCookie<string>(CookieNameEnum.TOKEN, { maxAge: TOKEN_MAX_AGE_SECONDS }).value =
 			userToken;
 		const { data: user } = await apiRouter.user.get.useQuery({ data: { userToken } });
-		console.log(user);
 		if (!user.value) {
 			console.error('No user data retrieved');
 			return;
 		}
-		useCookie<UserInfo | null>(CookieNameEnum.TG_USER, {
+		useCookie<UserInfoWithId | null>(CookieNameEnum.TG_USER, {
 			maxAge: TOKEN_MAX_AGE_SECONDS
 		}).value = user.value;
 	}

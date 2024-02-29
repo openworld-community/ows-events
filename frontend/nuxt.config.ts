5d9faaf9e3917ac3b955bf89067a2527a398b4d1
helloitsme9112 <daniamorozoff@mail.ru>
2024-02-29 17:20:00 +0300

    merged with origin radix branch

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,7 +3,7 @@ import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 import path from 'path';
 import { fileURLToPath, URL } from 'node:url';
 import { searchForWorkspaceRoot } from 'vite';
-import { BASE_URL, VITE_GOOGLE_OAUTH_KEY } from './constants/url';
+import { VITE_GOOGLE_OAUTH_KEY } from './constants/url';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
--- frontend/pages/event/create.vue
+++ frontend/pages/event/create.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import { useLocationStore } from '@/stores/location.store';
 
 const locationStore = useLocationStore();

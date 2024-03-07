4343486f2cb6c8c9f8c2280fcad128bde4174fc4
helloitsme9112 <daniamorozoff@mail.ru>
2024-03-07 17:26:04 +0300

    header button fix && filter icons fix

--- frontend/components/header/Common.vue
+++ frontend/components/header/Common.vue
@@ -1,4 +1,7 @@
-<script setup lang="ts">
+<script
+	setup
+	lang="ts"
+>
 import { SeoItempropNavEnum, SeoItemTypeEnum } from '../../constants/enums/seo';
 import { RouteNameEnum, RoutePathEnum } from '../../constants/enums/route';
 import { getRouteName } from '../../utils';
@@ -49,9 +52,10 @@ const titleOnMobile = computed(() => {
 const scrollToTop = () => window.scrollTo({ top: 0, behavior: 'smooth' });
 
 const goBack = () => {
-	if (router.options.history.state.back) {
-		router.back();
-	} else if (
+	// if (router.options.history.state.back) {
+	// 	router.back();
+	// } 
+	if (
 		getRouteName(route.name as string).includes(RouteNameEnum.USER_FAVOURITES) ||
 		getRouteName(route.name as string).includes(RouteNameEnum.USER_MY_EVENTS)
 	) {
@@ -75,10 +79,9 @@ const goBack = () => {
 		>
 			<div class="header__left">
 				<CommonButton
-					v-if="
-						hasBackButton &&
-						localePath(route.path) !== localePath({ path: RoutePathEnum.USER_PAGE })
-					"
+					v-if="hasBackButton &&
+			localePath(route.path) !== localePath({ path: RoutePathEnum.USER_PAGE })
+			"
 					is-icon
 					icon-name="back"
 					button-kind="ordinary"
@@ -89,12 +92,10 @@ const goBack = () => {
 					:is="logoComponentIs"
 					v-else
 					class="header__logo"
-					:title="
-						$t(isAtHome ? 'header.logo.at_home_aria' : 'header.logo.other_page_aria')
-					"
-					:aria-label="
-						$t(isAtHome ? 'header.logo.at_home_aria' : 'header.logo.other_page_aria')
-					"
+					:title="$t(isAtHome ? 'header.logo.at_home_aria' : 'header.logo.other_page_aria')
+			"
+					:aria-label="$t(isAtHome ? 'header.logo.at_home_aria' : 'header.logo.other_page_aria')
+			"
 					:to="!isAtHome ? localePath(RoutePathEnum.HOME) : undefined"
 					:itemprop="SeoItempropNavEnum.URL"
 					@click="isAtHome && scrollToTop()"
@@ -163,18 +164,20 @@ const goBack = () => {
 					:link="localePath(RoutePathEnum.USER_PAGE)"
 					button-kind="ordinary"
 					icon-name="user"
-					:button-text="
-						userStore.isAuthorized
-							? $t('header.navigation.user')
-							: $t('header.navigation.authorize')
-					"
+					:button-text="userStore.isAuthorized
+				? $t('header.navigation.user')
+				: $t('header.navigation.authorize')
+			"
 				/>
 			</div>
 		</nav>
 	</header>
 </template>
 
-<style scoped lang="less">
+<style
+	scoped
+	lang="less"
+>
 .header {
 	width: 100%;
 	height: var(--header-height);

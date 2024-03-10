966ad43b298a313ac340b52cd663d3f0b4cacf26
Sinamon-maker <nikaterina@gmail.com>
2024-03-10 04:26:00 -0700

    created new list of tags

--- frontend/i18n/locales/en-GB/event.ts
+++ frontend/i18n/locales/en-GB/event.ts
@@ -26,7 +26,18 @@ export default {
 		[Tags.SPORT]: 'Sport',
 		[Tags.STANDUP]: 'Stand-up',
 		[Tags.THEATER]: 'Theatre',
-		[Tags.TOURISM]: 'Tourism'
+		[Tags.TOURISM]: 'Tourism',
+		[Tags.FESTIVALS]: 'Festival',
+		[Tags.EXHIBITIONS]: 'Exhibition',
+		[Tags.LECTIONS_AND_MASTERCLASSESS]: 'Lecture and masterclass',
+		[Tags.PARTIES]: 'Parties',
+		[Tags.BUSINESS_AND_NETWORKING]: 'Business and networking',
+		[Tags.TOURISM_AND_EXCURSION]: 'Tourism and excursion',
+		[Tags.BOARD_GAMES_AND_QIZZE]: 'Board games and quizze',
+		[Tags.TASTINGS]: 'Tastings',
+		[Tags.RESTOURANT]: 'Restaurant',
+		[Tags.FAIRS_AND_CITY_EVENTS]: 'Fairs and city events',
+		[Tags.CONCERT_AND__SHOW]: 'Concert and show'
 	},
 	filteredEvents: {
 		no_events_found: 'No events found'
--- frontend/i18n/locales/ru-RU/event.ts
+++ frontend/i18n/locales/ru-RU/event.ts
@@ -1,6 +1,5 @@
 import { Tags } from '../../../../common/const/tags';
 
-
 export default {
 	image: {
 		event: 'Изображение мероприятия'
@@ -13,7 +12,7 @@ export default {
 		to: 'До'
 	},
 	tags: {
-		[Tags.ADULT]: 'Для взрослых',
+		[Tags.ADULT]: '18+',
 		[Tags.CHILDREN]: 'Для детей',
 		[Tags.CONCERT]: 'Концерт',
 		[Tags.CONFERENCE]: 'Конференция',
@@ -28,7 +27,18 @@ export default {
 		[Tags.SPORT]: 'Спорт',
 		[Tags.STANDUP]: 'Стендап',
 		[Tags.THEATER]: 'Театр',
-		[Tags.TOURISM]: 'Туризм'
+		[Tags.TOURISM]: 'Туризм',
+		[Tags.FESTIVALS]: 'Фестивали',
+		[Tags.EXHIBITIONS]: 'Выставки',
+		[Tags.LECTIONS_AND_MASTERCLASSESS]: 'Лекции и мастерклассы',
+		[Tags.PARTIES]: 'Вечеринки',
+		[Tags.BUSINESS_AND_NETWORKING]: 'Бизнес и Нетворкинг',
+		[Tags.TOURISM_AND_EXCURSION]: 'Туры и Экскурсии',
+		[Tags.BOARD_GAMES_AND_QIZZE]: 'Настольные игры и Квизы',
+		[Tags.TASTINGS]: 'Дегустации',
+		[Tags.RESTOURANT]: 'Рестораны',
+		[Tags.FAIRS_AND_CITY_EVENTS]: 'Ярмарки и городские мероприятия',
+		[Tags.CONCERT_AND__SHOW]: 'Концерты и шоу'
 	},
 	filteredEvents: {
 		no_events_found: 'Мероприятий не найдено'

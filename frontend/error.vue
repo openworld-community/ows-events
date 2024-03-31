dc287f0560651f9cba79b517c159db655813cf26
toprogramm <toprogramm@gmail.com>
2024-03-31 22:50:13 +0600

    trash cleaning, styles

--- frontend/error.vue
+++ frontend/error.vue
@@ -2,69 +2,38 @@
 import type { NuxtError } from '#app';
 import { RoutePathEnum } from '~/constants/enums/route';
 import { CookieNameEnum } from './constants/enums/common';
-// import type { UserInfoWithId } from './../common/types/user';
-// import { TOKEN_MAX_AGE_SECONDS } from './constants/defaultValues/time';
-const langCookie = useCookie(CookieNameEnum.LOCALE);
+import Error404 from './components/error/Error404.vue'
 
+const langCookie = useCookie(CookieNameEnum.LOCALE);
 const localePath = useLocalePath();
+
 
 const props = defineProps({
 	error: Object as () => NuxtError
 });
 
+
 const handleError = () =>
 	clearError({ redirect: localePath(RoutePathEnum.HOME, langCookie.value ?? 'ru') });
-
-// definePageMeta({
-// 	middleware: async (to, from) => {
-// 		const userToken = getFirstParam(to.params.id);
-// 		useCookie<string>(CookieNameEnum.TOKEN, { maxAge: TOKEN_MAX_AGE_SECONDS }).value =
-// 			userToken;
-// 		const { data: user } = await apiRouter.user.get.useQuery({ data: { userToken } });
-// 		if (!user.value) {
-// 			console.error('No user data retrieved');
-// 			return;
-// 		}
-// 		useCookie<UserInfoWithId | null>(CookieNameEnum.TG_USER, {
-// 			maxAge: TOKEN_MAX_AGE_SECONDS
-// 		}).value = user.value;
-// 	}
-// });
-await navigateTo(localePath(RoutePathEnum.HOME, langCookie.value ?? 'ru')) //navigateTo(localePath(RoutePathEnum.HOME, userLanguage));
-
-
 </script>
 <template>
 	<NuxtLayout>
 		<div class="service-error">
 			<template v-if="error.statusCode === 404">
-				<div class="page-error"></div>
-				<p>
-					<strong>Error code: {{ error.statusCode }}</strong>
-				</p>
+				<Error404 />
 			</template>
 			<template v-else>
-				<h1>OTHER ERROR</h1>
-				<p>
-					<strong>Error code: {{ error.statusCode }}</strong>
-				</p>
-			</template>
-			<p>
-				<NuxtLink
-					@click="handleError"
-					class="go-home"
-				>
-					{{ $t('errors.ERROR_PAGE_GO_HOME') }}
-				</NuxtLink>
-			</p>
-			<!-- <CommonButton
-
-				link=nav
-				class="navigation-menu-mobile__edit-button"
-				button-kind="ordinary"
-				:button-text="$t('errors.ERROR_PAGE_GO_HOME')"
-				icon-name="edit"
-			/> -->
+				ERROR 500 HERE
+			</template>			
+				<div class="go-home-wrapper">
+					<button
+						@click.native="handleError"
+						class="go-home"
+						tabindex="0" 
+					>
+						{{ $t('errors.ERROR_PAGE_GO_HOME', ) }}
+					</button>
+				</div>
 		</div>
 	</NuxtLayout>
 </template>
@@ -77,18 +46,19 @@ await navigateTo(localePath(RoutePathEnum.HOME, langCookie.value ?? 'ru')) //nav
 	align-items: center;
 	justify-content: center;
 }
-
-.page-error {
-	width: 500px;
-	height: 500px;
-	background-image: url(@/assets/img/error/error-404.png); /* путь к изображению */
-    background-size: contain; /* изображение будет масштабироваться, чтобы покрыть всю область фона блока */
-    background-repeat: no-repeat;
-
+.go-home-wrapper{
+	padding: 40px;
 }
-
 .go-home {
-	color: forestgreen;
-	font-weight: 500;
+  color: var(--color-accent-green-main);
+  font-weight: 500;
+  text-decoration: underline;
+  padding:5px 5px 8px 5px;
+  border-radius: 10px;
+
+  &:focus {
+    outline: none;
+    box-shadow: 0 0 5px var(--color-accent-green-main);
+  }
 }
 </style>

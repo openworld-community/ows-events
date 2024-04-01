1b670a5f8843bacc531253d9e50c0e97dba89975
toprogramm <toprogramm@gmail.com>
2024-04-01 13:50:31 +0600

    styling

--- frontend/components/error/Error404.vue
+++ frontend/components/error/Error404.vue
@@ -1,76 +1,73 @@
 <template>
-    <div>
-        <div class="page-error">
-            <div class="page-not-found-error">
-                <div class="not-found-style">
-                    <div class="stylesss">{{ $t('errors.ERROR_PAGE_PAGE', ) }}</div>
-                    <div class="stylesss">{{ $t('errors.ERROR_PAGE_NOT_FOUND', ) }}</div>
-                </div>            
-            </div>
-        
-            <div class="image-error"></div>
+	<div>
+		<div class="error-page">
+			<div class="error-page-not-found">
+				<div class="error-page-not-found--styling">
+					<div class="error-message-text">{{ $t('errors.ERROR_PAGE_PAGE') }}</div>
+					<div class="error-message-text">{{ $t('errors.ERROR_PAGE_NOT_FOUND') }}</div>
+				</div>
+			</div>
 
-            
-        </div>
-        <div class="stylesss stylesss--padding">{{ $t('errors.ERROR_PAGE_ERROR', ) }}</div>
-    </div>    
+			<div class="error-image"></div>
+		</div>
+		<div class="error-message-text error-message-text--padding">
+			{{ $t('errors.ERROR_PAGE_ERROR') }}
+		</div>
+	</div>
 </template>
 <style scoped lang="less">
-.stylesss {
-    /* Font Family импортируется в nuxt.config.ts в самом низу ориентир rel:stylesheet */
-    font-family: var(--font-family-error);
-    font-size:30px;
-    @media (max-width: 768px) {
+.error-message-text {
+	/* --font-family-error импортируется в nuxt.config.ts в самом низу ориентир rel:stylesheet */
+	font-family: var(--font-family-error);
+	font-size: 30px;
+	@media (max-width: 768px) {
 		font-size: 20px;
-        font-weight: 600;
-	}	
-    &--padding{
-        display: block;
-        text-align: center;
-        padding-top:10px;
-    }
+		font-weight: 600;
+	}
+	&--padding {
+		display: block;
+		text-align: center;
+		padding-top: 10px;
+	}
 }
-.page-error {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+.error-page {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
 	width: 550px;
 	height: 410px;
-    @media (max-width: 768px) {
+	@media (max-width: 768px) {
 		width: 340px;
-        height: 250px;
-	}	
+		height: 250px;
+	}
 }
-.image-error {
-    width: 550px;
+.error-image {
+	width: 550px;
 	height: 410px;
-    background-image: url(@/assets/img/error/404.webp); 
-    background-size: contain; /* изображение будет масштабироваться, чтобы покрыть всю область фона блока */
-    background-repeat: no-repeat;
-    @media (max-width: 768px) {
+	background-image: url(@/assets/img/error/404.webp);
+	background-size: contain; /* изображение будет масштабироваться, чтобы покрыть всю область фона блока */
+	background-repeat: no-repeat;
+	@media (max-width: 768px) {
 		width: 340px;
-        height: 250px;
-	}	
+		height: 250px;
+	}
 }
-.page-not-found-error{
-    position:fixed;
-    margin-bottom: 205px;
-    @media (max-width: 768px) {
+.error-page-not-found {
+	position: fixed;
+	margin-bottom: 205px;
+	@media (max-width: 768px) {
 		margin-bottom: 130px;
-	}	
-    
+	}
+	&--styling {
+		width: 550px;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		padding: 10px;
+		@media (max-width: 768px) {
+			width: 340px;
+		}
+	}
 }
-.not-found-style{
-    width: 550px;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    padding: 10px;
-    @media (max-width: 768px) {
-		width: 340px;
-	} 
-}
-
-
 </style>

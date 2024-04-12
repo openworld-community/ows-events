279f3ff880a79dfc0e4834266ceea9551f1c5818
toprogramm <toprogramm@gmail.com>
2024-04-12 18:55:52 +0600

    added lists

---
+++ frontend/components/common/cooperation/cooperation-lists/AllAdvantages.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import Advantage from '../Advantage.vue';
+</script>
+
+<template>
+	<Advantage
+		:name="$t('cooperation.hello.advantages.allEvents.name')"
+		:description="$t('cooperation.hello.advantages.allEvents.description')"
+	/>
+	<Advantage
+		:name="$t('cooperation.hello.advantages.simpleCreating.name')"
+		:description="$t('cooperation.hello.advantages.simpleCreating.description')"
+	/>
+	<Advantage
+		:name="$t('cooperation.hello.advantages.individualSearch.name')"
+		:description="$t('cooperation.hello.advantages.individualSearch.description')"
+	/>
+	<Advantage
+		:name="$t('cooperation.hello.advantages.ourSupport.name')"
+		:description="$t('cooperation.hello.advantages.ourSupport.description')"
+	/>
+	<Advantage
+		:name="$t('cooperation.hello.advantages.eventPages.name')"
+		:description="$t('cooperation.hello.advantages.eventPages.description')"
+	/>
+</template>

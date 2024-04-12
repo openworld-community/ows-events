1cce20d3ca240865e3b77ed39309ca2da2636189
toprogramm <toprogramm@gmail.com>
2024-04-12 18:57:33 +0600

    changed on components

--- frontend/pages/cooperation.vue
+++ frontend/pages/cooperation.vue
@@ -1,39 +1,28 @@
 <script setup lang="ts">
-import cooperation from "~/i18n/locales/en-GB/cooperation";
-import Package from "../components/common/cooperation/Package.vue"
+import Package from '../components/common/cooperation/Package.vue';
+import AllAdvantages from '../components/common/cooperation/cooperation-lists/AllAdvantages.vue';
+import AllPackages from '../components/common/cooperation/cooperation-lists/AllPackages.vue';
+import AllAdditionalServices from '../components/common/cooperation/cooperation-lists/AllAdditionalServices.vue';
+
 const { t } = useI18n();
 const mobile = inject('mobile');
-
 </script>
 
 <template>
 	<div class="root">
 		<HeaderCommon />
 		<div class="cooperation">
-			<div class="cooperation__cooperate">Сотрудничество с нами</div>
-			<div class="cooperation__placeholder">Placeholder</div>
-			<div class="wrapper">
-				<div class="cooperation__packages">Наши услуги</div>
-				<Package
-                :name="$t('cooperation.package.basic.name')"
-                :option="$t('cooperation.package.basic.option')"
-                :price="$t('cooperation.package.basic.price')"
-                />
-				<div class="package">Табличка с li</div>
-				<div class="package">Табличка с li</div>
-			</div>
+			<div class="cooperation__hello">{{ $t('cooperation.hello.name') }}</div>
+			<div class="cooperation__description">{{ $t('cooperation.hello.description') }}</div>
+			<AllAdvantages />
+			<AllPackages />
+			<AllAdditionalServices />
 		</div>
 		<FooterCommon v-if="!mobile" />
 	</div>
 </template>
 
 <style lang="less" scoped>
-// li::before {
-//     // такой синтаксис был выбран по причине того что если мы добавляем к ul оно не сработает
-//     // т.к выше list-style: none, а поменяв выше логику можно что то сломать.
-//     content: "•"; /* Устанавливаем содержимое псевдоэлемента как точку */
-//     margin-right: 5px; /* Добавляем отступ между маркером и текстом */
-// }
 .cooperation {
 	height: 100%;
 	width: 100vw;
@@ -46,12 +35,6 @@ const mobile = inject('mobile');
 	@media (max-width: 768px) {
 		padding-left: 1.5vw;
 		padding-right: 1.5vw;
-	}
-	.wrapper {
-		width: 100%;
-		display: flex;
-		flex-direction: column;
-		gap: 60px;
 	}
 }
 </style>

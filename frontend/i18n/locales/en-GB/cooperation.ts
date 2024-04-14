99b5048287d6448ac3b9b12b23905e0d7aacedad
toprogramm <toprogramm@gmail.com>
2024-04-15 02:31:39 +0600

    naming updated

--- frontend/i18n/locales/en-GB/cooperation.ts
+++ frontend/i18n/locales/en-GB/cooperation.ts
@@ -1,37 +1,41 @@
+import event from './event';
 export default {
-	title: {
-		name: 'Join the Balkan event aggregation platform',
-		description: `Our efforts are focused on making the cultural and public space of the Balkans - from Serbia to Montenegro - more accessible and united. We offer the opportunity for event organizers and participants to be part of this transformation through our innovative platform. That's why choosing our startup is a choice in favor of opportunities and advantages:`,
-		advantages: {
-			allEvents: {
-				name: 'All events in the region under one roof',
-				description:
-					'Our platform offers aggregation of all events of selected categories [Concerts and shows, Festivals, Exhibitions, Theater, Lectures and master classes, Tours and excursions, Sports, 18+, Tastings, Board games and quizzes, Fairs and city events, For children, Parties, Business and Networking, Restaurants] in the Balkans. This means users can find any event, from parties to business conferences, without leaving our site.'
-			},
-			simpleCreating: {
-				name: 'Easy to create and promote events',
-				description:
-					'We offer intuitive tools for quickly and easily creating events of any format. This allows organizers to easily share information about their events and attract more participants.'
-			},
-			individualSearch: {
-				name: 'Individual approaches to search',
-				description:
-					'Using our convenient filters by topic/tag, country and city, users can easily find events that match their interests and preferences.'
-			},
-			ourSupport: {
-				name: 'Support and care at every step',
-				description:
-					'Our caring support team is always available to help with any questions or concerns. We value every user and strive to provide the best user experience.'
-			},
-			eventPages: {
-				name: 'Event Detail Pages',
-				description:
-					'Each event on our platform is accompanied by a detailed description and convenient links for communication, which ensures effective interaction between organizers and participants.'
-			}
+	header: {
+		title: 'Join the Balkan event aggregation platform',
+		description: `Our efforts are focused on making the cultural and public space of the Balkans - from Serbia to Montenegro - more accessible and united. We offer the opportunity for event organizers and participants to be part of this transformation through our innovative platform. That's why choosing our startup is a choice in favor of opportunities and advantages:`
+	},
+	advantages: {
+		allEvents: {
+			title: 'All events in the region under one roof',
+			description: `Our platform offers aggregation of all events of selected categories (${Object.values(
+				event.tags
+			).join(
+				', '
+			)}) in the Balkans. This means users can find any event, from parties to business conferences, without leaving our site.`
+		},
+		simpleCreating: {
+			title: 'Easy to create and promote events',
+			description:
+				'We offer intuitive tools for quickly and easily creating events of any format. This allows organizers to easily share information about their events and attract more participants.'
+		},
+		individualSearch: {
+			title: 'Individual approaches to search',
+			description:
+				'Using our convenient filters by topic/tag, country and city, users can easily find events that match their interests and preferences.'
+		},
+		ourSupport: {
+			title: 'Support and care at every step',
+			description:
+				'Our caring support team is always available to help with any questions or concerns. We value every user and strive to provide the best user experience.'
+		},
+		eventPages: {
+			title: 'Event Detail Pages',
+			description:
+				'Each event on our platform is accompanied by a detailed description and convenient links for communication, which ensures effective interaction between organizers and participants.'
 		}
 	},
 	whyUs: {
-		name: 'Why choose us?',
+		title: 'Why choose us?',
 		description1:
 			'Choosing our startup is not only about access to a unique event aggregation platform. This is your contribution to the creation of a connecting cultural space in the Balkans, where every event becomes accessible and meaningful. We strive to advance culture and society through technology, inviting you to join our movement.',
 		description2: `Be part of our mission. Let's work together to make the cultural and public space of the Balkans more open and integrated. Join us today!`
@@ -39,7 +43,7 @@ export default {
 	ourServices: 'Our services',
 	package: {
 		basic: {
-			name: 'Basic package',
+			title: 'Basic package',
 			image: 'link',
 			option: [
 				'Posting an event announcement on the website',
@@ -50,7 +54,7 @@ export default {
 			button: 'Select'
 		},
 		standard: {
-			name: 'Standard Package',
+			title: 'Standard Package',
 			image: 'link',
 			option: [
 				'All services of the Basic package',
@@ -61,7 +65,7 @@ export default {
 			button: 'Select'
 		},
 		premium: {
-			name: 'Premium Package',
+			title: 'Premium Package',
 			image: 'link',
 			option: [
 				'All services of the Standard package',
@@ -74,32 +78,35 @@ export default {
 		}
 	},
 	additionalServices: {
-		name: 'Additional services',
+		title: 'Additional services',
 		socialMedia: {
-			name: 'Social Media:',
+			title: 'Social Media:',
 			image: 'link',
 			description: `Creating and posting posts about the event on the platform's social networks.`,
 			price: '$12000',
 			button: 'Select'
 		},
 		targetedAdvertising: {
-			name: 'Targeted advertising:',
+			title: 'Targeted advertising:',
 			image: 'link',
-			description: 'Launch targeted advertising campaigns on social networks and other platforms to attract the target audience.',
+			description:
+				'Launch targeted advertising campaigns on social networks and other platforms to attract the target audience.',
 			price: '$12000',
 			button: 'Select'
 		},
 		videoContent: {
-			name: 'Video content:',
+			title: 'Video content:',
 			image: 'link',
-			description: 'Production and placement of promotional video of the event on the website and social networks.',
+			description:
+				'Production and placement of promotional video of the event on the website and social networks.',
 			price: '$12000',
 			button: 'Select'
 		},
 		interactiveElements: {
-			name: 'Interactive elements:',
+			title: 'Interactive elements:',
 			image: 'link',
-			description: 'Organizing online voting, surveys, or competitions to increase engagement.',
+			description:
+				'Organizing online voting, surveys, or competitions to increase engagement.',
 			price: '$12000',
 			button: 'Select'
 		}

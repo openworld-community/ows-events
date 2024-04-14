6898dcf69cda979a61c714f6e7963e16d087db15
toprogramm <toprogramm@gmail.com>
2024-04-15 02:31:57 +0600

    minor changes

--- frontend/pages/cooperation.vue
+++ frontend/pages/cooperation.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import AdvantagesList from '../components/common/cooperation/cooperation-lists/AdvantagesList.vue';
-import PackagesList from '../components/common/cooperation/cooperation-lists/PackagesList.vue';
-import AdditionalsList from '../components/common/cooperation/cooperation-lists/AdditionalsList.vue';
+import AdvantagesList from '../components/cooperation/cooperation-lists/AdvantagesList.vue';
+import PackagesList from '../components/cooperation/cooperation-lists/PackagesList.vue';
+import AdditionalsList from '../components/cooperation/cooperation-lists/AdditionalsList.vue';
+import WhyUs from '../components/cooperation/WhyUs.vue';
 
-const { t } = useI18n();
 const mobile = inject('mobile');
 </script>
 
@@ -12,9 +12,10 @@ const mobile = inject('mobile');
 		<HeaderCommon />
 		<div class="cooperation">
 			<div class="cooperation__wrapper">
-				<h1 class="cooperation__title">{{ $t('cooperation.title.name') }}</h1>
-				<p class="cooperation__description">{{ $t('cooperation.title.description') }}</p>
+				<h1 class="cooperation__title">{{ $t('cooperation.header.title') }}</h1>
+				<p class="cooperation__description">{{ $t('cooperation.header.description') }}</p>
 				<AdvantagesList />
+				<WhyUs />
 				<PackagesList />
 				<AdditionalsList />
 			</div>
@@ -27,11 +28,15 @@ const mobile = inject('mobile');
 .cooperation {
 	display: flex;
 	justify-content: center;
-	padding-left: 5vw;
-	padding-right: 5vw;
+	padding-left: 7vw;
+	padding-right: 7vw;
 	@media (max-width: 768px) {
 		padding-left: 30px;
 		padding-right: 30px;
+	}
+	@media (max-width: 375px) {
+		padding-left: 12px;
+		padding-right: 12px;
 	}
 	&__wrapper {
 		width: 100%;
@@ -39,7 +44,10 @@ const mobile = inject('mobile');
 		padding-top: 50px;
 		display: flex;
 		flex-direction: column;
-		gap: 30px;
+		gap: 50px;
+		@media (max-width: 768px) {
+			gap: 30px;
+		}
 	}
 	&__title {
 		line-height: 1.3;
@@ -52,7 +60,8 @@ const mobile = inject('mobile');
 		}
 	}
 	&__description {
-		font-size: --font-size-L;
+		font-size: var(--font-size-L);
+		line-height: 1.5;
 	}
 }
 </style>

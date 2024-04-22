29fb40d41a798489d735311ecdbdbf30d9f4515f
toprogramm <toprogramm@gmail.com>
2024-04-23 01:01:34 +0600

    pricing const added

--- frontend/components/cooperation/items/index.ts
+++ frontend/components/cooperation/items/index.ts
@@ -1,110 +1,111 @@
+import { COOPERATION_PRICING } from '~/constants/defaultValues/cooperation-pricing';
 export type Advantage = {
-    title: string
-    description: string
-    image: string
-    isRevercedText: boolean
-}
+	title: string;
+	description: string;
+	image: string;
+	isRevercedText: boolean;
+};
 
 export type Additional = {
-    title: string
-    description: string
-    price: string
-    button: string
-    image: string
-}
+	title: string;
+	description: string;
+	price: number;
+	button: string;
+	image: string;
+};
 
 export type Services = {
-    title: string
-    options: string
-    price: string
-    button: string
-    image: string
-}
+	title: string;
+	options: string;
+	price: number;
+	button: string;
+	image: string;
+};
 
 export const advantage: Record<string, Advantage> = {
-    unity: {
-        title: 'cooperation.advantages.allEvents.title',
-        description: 'cooperation.advantages.allEvents.description',
-        image: '/img/cooperation/advantage/allEvents.png',
-        isRevercedText: false,
-    },
-    simplicity: {
-        title: 'cooperation.advantages.simpleCreating.title',
-        description: 'cooperation.advantages.simpleCreating.description',
-        image: '/img/cooperation/advantage/simpleCreating.png',
-        isRevercedText: true,
-    },
-    individuality: {
-        title: 'cooperation.advantages.individualSearch.title',
-        description: 'cooperation.advantages.individualSearch.description',
-        image: '/img/cooperation/advantage/individualSearch.png',
-        isRevercedText: false,
-    },
-    care: {
-        title: 'cooperation.advantages.ourSupport.title',
-        description: 'cooperation.advantages.ourSupport.description',
-        image: '/img/cooperation/advantage/ourSupport.png',
-        isRevercedText: true,
-    },
-    pages: {
-        title: 'cooperation.advantages.eventPages.title',
-        description: 'cooperation.advantages.eventPages.description',
-        image: '/img/cooperation/advantage/eventPages.png',
-        isRevercedText: false,
-    }
-}
+	unity: {
+		title: 'cooperation.advantages.allEvents.title',
+		description: 'cooperation.advantages.allEvents.description',
+		image: '/img/cooperation/advantage/allEvents.png',
+		isRevercedText: false
+	},
+	simplicity: {
+		title: 'cooperation.advantages.simpleCreating.title',
+		description: 'cooperation.advantages.simpleCreating.description',
+		image: '/img/cooperation/advantage/simpleCreating.png',
+		isRevercedText: true
+	},
+	individuality: {
+		title: 'cooperation.advantages.individualSearch.title',
+		description: 'cooperation.advantages.individualSearch.description',
+		image: '/img/cooperation/advantage/individualSearch.png',
+		isRevercedText: false
+	},
+	care: {
+		title: 'cooperation.advantages.ourSupport.title',
+		description: 'cooperation.advantages.ourSupport.description',
+		image: '/img/cooperation/advantage/ourSupport.png',
+		isRevercedText: true
+	},
+	pages: {
+		title: 'cooperation.advantages.eventPages.title',
+		description: 'cooperation.advantages.eventPages.description',
+		image: '/img/cooperation/advantage/eventPages.png',
+		isRevercedText: false
+	}
+};
 
 export const services: Record<string, Services> = {
-    base: {
-        title: 'cooperation.package.basic.title',
-        options: 'cooperation.package.basic.option',
-        price: 'cooperation.package.basic.price',
-        button: 'cooperation.package.basic.button',
-        image: '/img/cooperation/package/basic.png',
-    },
-    exntended: {
-        title: 'cooperation.package.standard.title',
-        options: 'cooperation.package.standard.option',
-        price: 'cooperation.package.standard.price',
-        button: 'cooperation.package.standard.button',
-        image: '/img/cooperation/package/standard.png',
-    },
-    premium: {
-        title: 'cooperation.package.premium.title',
-        options: 'cooperation.package.premium.option',
-        price: 'cooperation.package.premium.price',
-        button: 'cooperation.package.premium.button',
-        image: '/img/cooperation/package/premium.png',
-    },
-}
+	basic: {
+		title: 'cooperation.service.basic.title',
+		options: 'cooperation.service.basic.option',
+		price: COOPERATION_PRICING.services.basic.price,
+		button: 'cooperation.service.basic.button',
+		image: '/img/cooperation/service/basic.png'
+	},
+	standard: {
+		title: 'cooperation.service.standard.title',
+		options: 'cooperation.service.standard.option',
+		price: COOPERATION_PRICING.services.standard.price,
+		button: 'cooperation.service.standard.button',
+		image: '/img/cooperation/service/standard.png'
+	},
+	premium: {
+		title: 'cooperation.service.premium.title',
+		options: 'cooperation.service.premium.option',
+		price: COOPERATION_PRICING.services.premium.price,
+		button: 'cooperation.service.premium.button',
+		image: '/img/cooperation/service/premium.png'
+	}
+};
 
 export const additional: Record<string, Additional> = {
-    social: {
-        title: 'cooperation.additionalServices.socialMedia.title',
-        description: 'cooperation.additionalServices.socialMedia.description',
-        price: 'cooperation.additionalServices.socialMedia.price',
-        button: 'cooperation.additionalServices.socialMedia.button',
-        image: '/img/cooperation/additional/socialMedia.png',
-    },
-    target: {
-        title: 'cooperation.additionalServices.targetedAdvertising.title',
-        description: 'cooperation.additionalServices.targetedAdvertising.description',
-        price: 'cooperation.additionalServices.targetedAdvertising.price',
-        button: 'cooperation.additionalServices.targetedAdvertising.button',
-        image: '/img/cooperation/additional/targetedAdvertising.png',
-    },
-    video: {
-        title: 'cooperation.additionalServices.videoContent.title',
-        description: 'cooperation.additionalServices.videoContent.description',
-        price: 'cooperation.additionalServices.videoContent.price',
-        button: 'cooperation.additionalServices.videoContent.button',
-        image: '/img/cooperation/additional/videoContent.png',
-    },
-    interactive: {
-        title: 'cooperation.additionalServices.interactiveElements.title',
-        description: 'cooperation.additionalServices.interactiveElements.description',
-        price: 'cooperation.additionalServices.interactiveElements.price',
-        button: 'cooperation.additionalServices.interactiveElements.button',
-        image: '/img/cooperation/additional/interactiveElements.png',
-    },
-}
+	social: {
+		title: 'cooperation.additionalServices.socialMedia.title',
+		description: 'cooperation.additionalServices.socialMedia.description',
+		price: COOPERATION_PRICING.additionalServices.social.price,
+		button: 'cooperation.additionalServices.socialMedia.button',
+		image: '/img/cooperation/additional/socialMedia.png'
+	},
+	target: {
+		title: 'cooperation.additionalServices.targetedAdvertising.title',
+		description: 'cooperation.additionalServices.targetedAdvertising.description',
+		price: COOPERATION_PRICING.additionalServices.target.price,
+		button: 'cooperation.additionalServices.targetedAdvertising.button',
+		image: '/img/cooperation/additional/targetedAdvertising.png'
+	},
+	video: {
+		title: 'cooperation.additionalServices.videoContent.title',
+		description: 'cooperation.additionalServices.videoContent.description',
+		price: COOPERATION_PRICING.additionalServices.video.price,
+		button: 'cooperation.additionalServices.videoContent.button',
+		image: '/img/cooperation/additional/videoContent.png'
+	},
+	interactive: {
+		title: 'cooperation.additionalServices.interactiveElements.title',
+		description: 'cooperation.additionalServices.interactiveElements.description',
+		price: COOPERATION_PRICING.additionalServices.interactive.price,
+		button: 'cooperation.additionalServices.interactiveElements.button',
+		image: '/img/cooperation/additional/interactiveElements.png'
+	}
+};

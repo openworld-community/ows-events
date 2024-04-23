3df308acad70b416f48d43b71293e64d56a0afc4
toprogramm <toprogramm@gmail.com>
2024-04-23 16:45:25 +0600

    translations

---
+++ frontend/i18n/locales/en-GB/support.ts
@@ -0,0 +1,7 @@
+export default {
+	title: `Hi! \n If you're here, you probably want to contact us or solve a problem.`,
+	message: 'Write to us by email or in our telegram bot, whichever is convenient for you. Control your destiny yourself! 😊',
+	email:'Email',
+    telegram: '@afisha_peredelano_support_bot',
+    question: 'I have a question'
+};

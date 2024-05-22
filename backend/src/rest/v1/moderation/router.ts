a1222f649ce0af9fc30e348b6d283134fce1f9e9
igor.la <igor.la@fora-soft.com>
2024-05-22 13:04:26 +0300

    Fix delete route isAuthor check; Rename auth helper function;

--- backend/src/rest/v1/moderation/router.ts
+++ backend/src/rest/v1/moderation/router.ts
@@ -2,24 +2,24 @@ import { FastifyInstance } from 'fastify';
 import { approve, decline, get } from './controller';
 import { IApproveEventRoute, IDeclineEventRoute, IGetEventsRoute } from './types';
 import { approveEventSchema, declineEventSchema, getEventsByStatusSchema } from './schema';
-import { authorizeUserHelper } from '../../../plugins/authorization';
+import { generateAuthUserFn } from '../../../plugins/authorization';
 import { UserRoles } from '../../../../../common/const/userRoles';
 
 export const manualModerationApi = async (fastify: FastifyInstance) => {
 	fastify.get<IGetEventsRoute>('/get/:status', {
-		preHandler: fastify.auth([authorizeUserHelper([UserRoles.MODERATOR, UserRoles.ADMIN])]),
+		preHandler: fastify.auth([generateAuthUserFn([UserRoles.MODERATOR, UserRoles.ADMIN])]),
 		schema: getEventsByStatusSchema,
 		handler: get
 	});
 
 	fastify.get<IApproveEventRoute>('/approve/:eventId', {
-		preHandler: fastify.auth([authorizeUserHelper([UserRoles.MODERATOR, UserRoles.ADMIN])]),
+		preHandler: fastify.auth([generateAuthUserFn([UserRoles.MODERATOR, UserRoles.ADMIN])]),
 		schema: approveEventSchema,
 		handler: approve
 	});
 
 	fastify.get<IDeclineEventRoute>('/decline/:eventId', {
-		preHandler: fastify.auth([authorizeUserHelper([UserRoles.MODERATOR, UserRoles.ADMIN])]),
+		preHandler: fastify.auth([generateAuthUserFn([UserRoles.MODERATOR, UserRoles.ADMIN])]),
 		schema: declineEventSchema,
 		handler: decline
 	});

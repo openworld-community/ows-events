7ede7268dea10bb0b9baf3f1ef0d88824806b09b
Nikita S <nik@mail.ru>
2024-06-05 00:36:31 +0500

    about_info

--- frontend/pages/about.vue
+++ frontend/pages/about.vue
@@ -5,8 +5,16 @@
 <template>
     <main class="about">
         <div class="about__info">
-
+            <div class="about__wrapper">
+                <img
+                    class="about__wrapper__image"
+                    src="@/assets/img/about/about-info.png"   
+		        />
+                <p class="about__wrapper__text-img">{{ $t('about.title') }}</p>
+            </div>
+            <p class="about__text">{{ $t('about.idea') }}</p>
         </div>
+
 
         <AboutValues />
         
@@ -17,5 +25,41 @@
     </main>
 </template>
 
-<style>
+<style lang="less" scoped>
+.about {
+    width: 100%;
+    padding: 50px;
+    
+    
+    &__info {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+    
+    }
+
+    &__wrapper {
+            position: relative;
+            
+
+            
+
+            &__text-img {
+                // Это для того чтобы расположить "О нас" посередине изображения
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+                font-size: 60px;
+
+            }
+
+        
+        }
+
+    
+}
+
+    
 </style>

4069f2bc5289a3912948b02d37b7e166e1e1a41d
Sinamon-maker <nikaterina@gmail.com>
2024-06-06 23:51:30 -0700

    removed unused composable

--- frontend/composables/useSendTrackingEvent.ts
+++
@@ -1,7 +0,0 @@
-export const useSendTrackingEvent = <T extends Record<string, string>>() => {
-	const sendAnalyticEvent = (eventName: string, parameters: T) => {
-		useTrackEvent(eventName, parameters);
-		console.log('send_Analytics');
-	};
-	return { sendAnalyticEvent };
-};

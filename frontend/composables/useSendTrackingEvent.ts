912964f30a4b17bf2e803ef4f8a6a44d4533f554
Sinamon-maker <nikaterina@gmail.com>
2024-06-07 10:15:20 -0700

    fixedgit add .
    :Q

--- frontend/composables/useSendTrackingEvent.ts
+++ frontend/composables/useSendTrackingEvent.ts
@@ -2,11 +2,11 @@ export const useSendTrackingEvent = () => {
 	const sendAnalytics = {
 		search: (name: 'view_search_results', params: SearchType) =>
 			useTrackEvent('view_search_results', params),
-		login: (name: 'click_login', params: LoginType) => useTrackEvent('try_login', params),
+		login: (name: 'click_login', params: LoginType) => useTrackEvent('click_login', params),
 		redirect: (name: 'redirect to event url', params: RedirectToEventType) =>
 			useTrackEvent('redirect to event url', params),
 		clickEvent: (name: 'click_on_event', params: ClickEventType) =>
-			useTrackEvent('redirect to event url', params),
+			useTrackEvent('click_on_event', params),
 		favourites: (name: 'add_favourites', params: FavouritesType) =>
 			useTrackEvent('add_favourites', params),
 		formEvent: (name: 'form_event', params: FormEventType) =>

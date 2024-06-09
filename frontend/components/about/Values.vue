31a5d56ed4c6eb919128ebe954f82ae742bcf703
Nikita S <nik@mail.ru>
2024-06-09 21:32:47 +0500

    new_component_values

---
+++ frontend/components/about/Values.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+
+defineProps({
+	imageUrl: {
+		type: String,
+		default: ''
+	},
+	title: {
+		type: String,
+		defualt: ''
+	},
+});
+
+
+</script>
+
+<template>
+    
+    <div>
+        <!--
+        <ul>
+            <AboutValuesItems 
+                v-for = "item in advantage"
+                v-bind = "item"
+                :key = "item.title"
+                class = "Values__item"
+            />
+        </ul>
+        -->
+        
+        <img :src="imageUrl" alt="openness">
+        <p> {{ title }}</p>
+        
+
+
+    </div>
+    
+</template>
+
+<style>
+
+
+</style>

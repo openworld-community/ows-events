6e8d68f33266bd4a0a1f76354ada0f8969316c43
Nikita S <nik@mail.ru>
2024-06-09 23:19:04 +0500

    component_values_end

--- frontend/components/about/ValuesItems.vue
+++ frontend/components/about/ValuesItems.vue
@@ -1,3 +1,35 @@
+<script setup lang="ts">
+
+    import type { values } from '.';
+    const props = defineProps<values>();
+
+</script>
+
+
 <template>
-    <li> 123</li>
+    <li class="ValuesItem__list">
+        <img :src="imageUrl" alt="openness">
+        <p class="ValuesItem__text"> {{ $t(title) }}</p>
+    </li>
+   
 </template>
+
+
+<style lang="less" scoped>
+.ValuesItem {
+
+    &__list {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+    }
+    
+	&__text {
+		font-size: var(--font-size-S);
+        width: 305px;
+        margin-top: 10px;
+       
+	}
+}
+</style>

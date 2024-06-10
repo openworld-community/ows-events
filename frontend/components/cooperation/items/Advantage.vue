8fa369399ad77ca7263302c816384670d81287a5
toprogramm <toprogramm@gmail.com>
2024-06-11 03:34:45 +0600

    cooperation fonts

--- frontend/components/cooperation/items/Advantage.vue
+++ frontend/components/cooperation/items/Advantage.vue
@@ -1,23 +1,23 @@
 <script setup lang="ts">
 import type { Advantage } from '.';
 
-const props = defineProps<Advantage>()
+const props = defineProps<Advantage>();
 
-const { width } = useWindowSize()
+const { width } = useWindowSize();
 
 const direction = computed(() => {
-    if (width.value > 900) {
-        return props.isRevercedText ? 'row-reverse' : 'row'
-    } else {
-        return 'column'
-    }
-})
+	if (width.value > 900) {
+		return props.isRevercedText ? 'row-reverse' : 'row';
+	} else {
+		return 'column';
+	}
+});
 
-const alignSelf = computed(() => props.isRevercedText ? 'flex-end' : 'flex-start') 
+const alignSelf = computed(() => (props.isRevercedText ? 'flex-end' : 'flex-start'));
 </script>
 
 <template>
-    <div class="advantage">
+	<div class="advantage">
 		<img
 			:src="image"
 			class="advantage__image"
@@ -31,37 +31,43 @@ const alignSelf = computed(() => props.isRevercedText ? 'flex-end' : 'flex-start
 
 <style scoped lang="less">
 .advantage {
-    display: flex;
-    flex-direction: v-bind(direction);
-    justify-content: space-between;
-    gap: 50px;
-    font-size: var(--font-size-L);
+	display: flex;
+	flex-direction: v-bind(direction);
+	justify-content: space-between;
+	gap: 50px;
+	font-size: var(--font-size-L);
+	@media (max-width: 768px) {
+		gap: 35px;
+		font-size: var(--font-size-ML);
+	}
 
-    &__image {
-        width: 315px;
-        height: 255px;
-        align-self: v-bind(alignSelf);
-        @media (max-width: 900px) {
-            align-self: center;
-        }
-    }
+	&__image {
+		width: 315px;
+		height: 255px;
+		align-self: v-bind(alignSelf);
+		@media (max-width: 900px) {
+			align-self: center;
+		}
+	}
 
-    &__text {
-        max-width: 66%;        
-        @media (max-width: 900px) {
-            max-width: none;
-        }
-    }
+	&__text {
+		max-width: 66%;
+		@media (max-width: 900px) {
+			max-width: none;
+		}
+	}
 
-    &__description{
-        line-height: 1.5;
-    }
+	&__description {
+		line-height: 1.5;
+	}
 
-    &__header {
-        margin-bottom: 35px;
-        @media (min-width: 375px) and (max-width: 768px) {
+	&__header {
+		margin-bottom: 35px;
+		font-size: var(--font-size-ML);
+		@media (min-width: 375px) and (max-width: 768px) {
+			font-size: var(--font-size-XL);
 			text-align: center;
 		}
-    }
+	}
 }
 </style>

a37974decbcd521dc9539563189ccc714aad3372
Nikita S <nik@mail.ru>
2024-06-20 00:07:40 +0500

    установил Pretter

--- frontend/components/about/ValuesItems.vue
+++ frontend/components/about/ValuesItems.vue
@@ -1,42 +1,35 @@
 <script setup lang="ts">
-
-    import type { AboutValuesData } from './AboutData';
-    defineProps<AboutValuesData>();
-
+import type { AboutValuesData } from './AboutData';
+defineProps<AboutValuesData>();
 </script>
 
-
 <template>
-    <li class="valuesItem">
-        <img 
-            class="valuesItem__image" 
-            :src="imageUrl" 
-        >
-        <p class="valuesItem__text"> {{ $t(title) }}</p>
-    </li>
-   
+	<li class="valuesItem">
+		<img
+			class="valuesItem__image"
+			:src="imageUrl"
+		/>
+		<p class="valuesItem__text">{{ $t(title) }}</p>
+	</li>
 </template>
-
 
 <style lang="less" scoped>
 .valuesItem {
-
 	&__text {
 		font-size: var(--font-size-S);
-        width: 300px;
-        margin-top: 10px;
+		width: 300px;
+		margin-top: 10px;
 
-        @media (min-width: 768px) {
+		@media (min-width: 768px) {
 			font-size: var(--font-size-L);
-            width: 40vw;  
-		} 
+			width: 40vw;
+		}
 	}
 
-    &__image {
-        @media (min-width: 440px) {
-            width: 40vw;
-		}  
-    }
+	&__image {
+		@media (min-width: 440px) {
+			width: 40vw;
+		}
+	}
 }
-
 </style>

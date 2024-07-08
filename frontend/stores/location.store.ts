ca6ad9b91943b70c6b1838c7fe6995507e1c6f94
igor.la <igor.la@fora-soft.com>
2024-07-08 12:40:46 +0300

    Change available countries and cities logic to request ones from server

--- frontend/stores/location.store.ts
+++ frontend/stores/location.store.ts
@@ -28,37 +28,36 @@ export const useLocationStore = defineStore('location', {
 		countries(state): LocationStore['_countries'] {
 			// Список всех стран:
 
-			// (async function () {
-			// 	if (process.server) return;
-			// 	if (state._countries.size) return;
-			//
-			// 	// otherwise Nuxt doesn't do request on client during initial hydration, I'm not smart enough to tell why
-			// 	await new Promise((r) => r(0));
-			//
-			// 	const { $locationStoreForage } = useNuxtApp();
-			// 	const localCountries: Country[] | null = await $locationStoreForage.getItem(
-			// 		COUNTRIES_KEY
-			// 	);
-			// 	if (localCountries) {
-			// 		if (!localCountries.length) return;
-			// 		state._countries = new Set(localCountries);
-			// 		return;
-			// 	}
-			//
-			// 	const { data } = await apiRouter.location.country.getAll.useQuery({});
-			// 	if (!data.value?.length) return;
-			//
-			// 	state._countries = new Set(data.value);
-			// 	// data.value is Proxy which can't copied to storage directly - spread operator converts back to native object
-			// 	$locationStoreForage.setItem(COUNTRIES_KEY, [...data.value]);
-			// })();
-			// return state._countries;
+			(async function () {
+				if (process.server) return;
 
-			// Сейчас добавляем страны вручную
-			const countries = ['Montenegro', 'Serbia'];
-
-			state._countries = new Set(countries);
+				// otherwise Nuxt doesn't do request on client during initial hydration, I'm not smart enough to tell why
+				await new Promise((r) => r(0));
+				//
+				// 	const { $locationStoreForage } = useNuxtApp();
+				// 	const localCountries: Country[] | null = await $locationStoreForage.getItem(
+				// 		COUNTRIES_KEY
+				// 	);
+				// 	if (localCountries) {
+				// 		if (!localCountries.length) return;
+				// 		state._countries = new Set(localCountries);
+				// 		return;
+				// 	}
+				//
+				const { data } = await apiRouter.location.country.getAll.useQuery({});
+				if (!data.value?.length) return;
+				//
+				state._countries = new Set(data.value);
+				// 	// data.value is Proxy which can't copied to storage directly - spread operator converts back to native object
+				// 	$locationStoreForage.setItem(COUNTRIES_KEY, [...data.value]);
+			})();
 			return state._countries;
+
+			// // Сейчас добавляем страны вручную
+			// const countries = ['Montenegro', 'Serbia'];
+			//
+			// state._countries = new Set(countries);
+			// return state._countries;
 		},
 		currencies(state): LocationStore['_currencies'] {
 			state._currencies = ['USD', 'EUR', 'RSD', 'BTC', 'USDT', 'USDC', 'ETH'];
@@ -135,28 +134,27 @@ export const useLocationStore = defineStore('location', {
 		},
 		getCitiesByCountry(country: Country) {
 			(async () => {
-				if (process.server) return;
+				// 	if (process.server) return;
 				if (this._citiesByCountry.get(country) || !this.countries.has(country)) return;
-
+				//
 				// forces Nuxt to await function calls if there are multiple of them(avoid duplication of requests)
 				await new Promise((r) => r(0));
-
-				const { $locationStoreForage } = useNuxtApp();
-				const localCities: City[] | null = await $locationStoreForage.getItem(country);
-				if (localCities) {
-					this._citiesByCountry.set(country, localCities);
-					return;
-				}
-
+				//
+				// 	const { $locationStoreForage } = useNuxtApp();
+				// 	const localCities: City[] | null = await $locationStoreForage.getItem(country);
+				// 	if (localCities) {
+				// 		this._citiesByCountry.set(country, localCities);
+				// 		return;
+				// 	}
+				//
 				const { data } = await apiRouter.location.country.getCities.useQuery({
 					data: { country }
 				});
 				if (!data.value) return;
 				this._citiesByCountry.set(country, data.value);
-				// data.value is Proxy which can't copied to storage directly - spread operator converts back to native object
-				$locationStoreForage.setItem(country, [...data.value]);
+				// 	// data.value is Proxy which can't copied to storage directly - spread operator converts back to native object
+				// 	$locationStoreForage.setItem(country, [...data.value]);
 			})();
-
 			return this._citiesByCountry.get(country) ?? [];
 		}
 	}

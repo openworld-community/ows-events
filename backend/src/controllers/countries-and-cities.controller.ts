b2afa1c42b5b9371c0509fec3fad0661ebf2c884
igor.la <igor.la@fora-soft.com>
2024-07-08 12:37:38 +0300

    Add localization into parser, location and events controllers

--- backend/src/controllers/countries-and-cities.controller.ts
+++ backend/src/controllers/countries-and-cities.controller.ts
@@ -1,33 +1,100 @@
-import fs from 'fs';
 import { EventModel } from '../models/event.model';
+import { CitiesModel } from '../models/cities.model';
+import { CommonErrorsEnum, SupportedLanguages, SupportedCountries } from '../../../common/const';
+import { CountriesForParserModel } from '../models/countries-for-parser.model';
+import { citiesForParserController } from './cities-for-parser.controller';
+import { countriesForParserController } from './countries-for-parser.controller';
 
 class CountriesAndCitiesController {
-	countries: string[];
-
-	citiesByCountry: { [key: string]: string[] } = {};
-
-	constructor() {
-		const map = JSON.parse(fs.readFileSync('./assets/countries.json', 'utf8'));
-
-		this.countries = Object.keys(map);
-		this.citiesByCountry = map;
+	async getLocalizedCities(lang: SupportedLanguages) {
+		const pipeline = [
+			{
+				$unset: ['countryCode', 'alternateNames', '_id']
+			}
+		];
+		const cities = await CitiesModel.aggregate(pipeline).exec();
+		return cities.map((city) => city[lang]);
 	}
 
-	getCountryByCity(city: string) {
-		const { countries } = this;
-		if (countries.includes(city)) return city;
-		const possibleCountries = countries.filter((country) =>
-			this.citiesByCountry[country].includes(city)
+	async getLocalizedCountries(lang: SupportedLanguages) {
+		const pipeline = [
+			{
+				$match: {
+					iso_3166_1_alpha_2_codes: {
+						$in: Object.values(SupportedCountries)
+					}
+				}
+			}
+		];
+		const countries = await CountriesForParserModel.aggregate(pipeline).exec();
+		return countries.map((country) => {
+			if (lang === SupportedLanguages.RUSSIAN) return country.russian_short!;
+			return country.english_short!;
+		});
+	}
+
+	async getLocalizedCityName(cityName: string, lang: SupportedLanguages) {
+		if (cityName === '') return '';
+		const city = await CitiesModel.findOne({
+			$or: [{ en: cityName }, { ru: cityName }, { alternateNames: cityName }]
+		});
+		if (!city) return cityName;
+		return city[lang];
+	}
+
+	async getLocalizedCountryName(countryName: string, lang: SupportedLanguages) {
+		if (countryName === '') return '';
+		const country = await CountriesForParserModel.findOne({
+			$or: [
+				{ english_short: countryName },
+				{ french_short: countryName },
+				{ spanish_short: countryName },
+				{ russian_short: countryName }
+			]
+		});
+		if (!country) throw new Error(CommonErrorsEnum.COUNTRY_NOT_FOUND);
+		if (lang === SupportedLanguages.RUSSIAN) return country.russian_short!;
+		return country.english_short!;
+	}
+
+	async getCountryCodeByName(countryName: string) {
+		if (countryName === '') return '';
+		const country = await CountriesForParserModel.findOne({
+			$or: [
+				{ english_short: countryName },
+				{ french_short: countryName },
+				{ spanish_short: countryName },
+				{ russian_short: countryName }
+			]
+		});
+		if (!country) throw new Error(CommonErrorsEnum.COUNTRY_NOT_FOUND);
+		return country.iso_3166_1_alpha_2_codes!;
+	}
+
+	async getCountryByCity(city: string) {
+		const countryCode = await citiesForParserController.findCountryByCityName(city);
+		if (!countryCode) throw new Error(CommonErrorsEnum.CITY_NOT_FOUND);
+		const countryName = await countriesForParserController.findEnglishCountryNameByCountryCode(
+			countryCode
 		);
-		return possibleCountries[0];
+		if (!countryName) throw new Error(CommonErrorsEnum.COUNTRY_NOT_FOUND);
+		return countryName;
 	}
 
-	getCitiesByCountry(country: string) {
-		const citiesByCountry = this.citiesByCountry[country];
-		return citiesByCountry;
+	async getCitiesByCountry(country: SupportedCountries, lang: SupportedLanguages) {
+		const pipeline = [
+			{
+				$match: {
+					countryCode: country
+				}
+			}
+		];
+
+		const cities = await CitiesModel.aggregate(pipeline).exec();
+		return cities.map((city) => city[lang]);
 	}
 
-	async getUsedCountries() {
+	async getUsedCountries(lang: SupportedLanguages) {
 		const countries: string[] = await EventModel.distinct('location.country', {
 			'meta.moderation.status': { $nin: ['declined', 'in-progress'] },
 			$expr: {
@@ -35,10 +102,12 @@ class CountriesAndCitiesController {
 			},
 			'location.country': { $nin: [''] }
 		});
-		return countries;
+		return Promise.all(
+			countries.map(async (country) => this.getLocalizedCountryName(country, lang))
+		);
 	}
 
-	async getUsedCitiesByCountry(country: string) {
+	async getUsedCitiesByCountry(country: string, lang: SupportedLanguages) {
 		const cities: string[] = await EventModel.distinct('location.city', {
 			'location.country': country,
 			'meta.moderation.status': { $nin: ['declined', 'in-progress'] },
@@ -47,10 +116,10 @@ class CountriesAndCitiesController {
 				$gte: [{ $add: ['$date', { $multiply: [1000, '$durationInSeconds'] }] }, Date.now()]
 			}
 		});
-		return cities;
+		return Promise.all(cities.map(async (city) => this.getLocalizedCityName(city, lang)));
 	}
 
-	async getUsedCities() {
+	async getUsedCities(lang: SupportedLanguages) {
 		const cities: string[] = await EventModel.distinct('location.city', {
 			'location.country': { $in: ['Serbia', 'Montenegro'] },
 			'meta.moderation.status': { $nin: ['declined', 'in-progress'] },
@@ -59,28 +128,7 @@ class CountriesAndCitiesController {
 				$gte: [{ $add: ['$date', { $multiply: [1000, '$durationInSeconds'] }] }, Date.now()]
 			}
 		});
-		return cities;
-	}
-
-	getRandomPair({ country, city }: { country?: string; city?: string }) {
-		const getRandomCountry = () => {
-			const randomCountryIndex = Math.floor(Math.random() * this.countries.length);
-			const randomCountry = this.countries[randomCountryIndex];
-			return randomCountry;
-		};
-
-		const getRandomCity = (randomCountry: string) => {
-			const randomCityIndex = Math.floor(
-				Math.random() * (this.citiesByCountry[randomCountry]?.length || 0)
-			);
-			const randomCity = this.citiesByCountry[randomCountry]?.[randomCityIndex];
-			return randomCity;
-		};
-
-		const newCountry = country || getRandomCountry();
-		const newCity = city || getRandomCity(newCountry);
-
-		return { country: newCountry, city: newCity };
+		return Promise.all(cities.map(async (city) => this.getLocalizedCityName(city, lang)));
 	}
 }
 

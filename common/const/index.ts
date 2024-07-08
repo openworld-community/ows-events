395054729d637ccd18e8355ca0f184236ca85d54
igor.la <igor.la@fora-soft.com>
2024-07-08 12:36:19 +0300

    Add supported countries enum

--- common/const/index.ts
+++ common/const/index.ts
@@ -1,6 +1,13 @@
 import { CommonErrorsEnum } from './common-errors';
 import { EventValidatorErrorTypes } from './event-validation-error';
 import { SupportedLanguages, SupportedLanguagesArray } from './supportedLanguages';
+import { SupportedCountries } from './supportedCountries';
 
-export { CommonErrorsEnum, EventValidatorErrorTypes, SupportedLanguages, SupportedLanguagesArray };
+export {
+	CommonErrorsEnum,
+	EventValidatorErrorTypes,
+	SupportedLanguages,
+	SupportedLanguagesArray,
+	SupportedCountries
+};
 export type ErrorCodes = EventValidatorErrorTypes | CommonErrorsEnum;
--- common/const/supportedCountries.ts
+++ common/const/supportedCountries.ts
@@ -1,4 +1,4 @@
-// values should be capitalized ISO 3166 1 alpha 2 code
+// Values should be capitalized ISO 3166 1 alpha 2 code
 export enum SupportedCountries {
 	MONTENEGRO = 'ME',
 	SERBIA = 'RS'

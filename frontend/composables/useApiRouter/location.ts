61af135d6e0ac81e537fcc88c454c69384142fb7
igor.la <igor.la@fora-soft.com>
2024-07-08 13:59:06 +0300

    Add city and country localization queries

--- frontend/composables/useApiRouter/location.ts
+++ frontend/composables/useApiRouter/location.ts
@@ -1,11 +1,19 @@
 import type { City, Country } from '~/stores/location.store';
-import { defineQuery, useBackendFetch } from './utils';
+import { defineMutation, defineQuery, useBackendFetch } from './utils';
 
 export const location = {
 	country: {
 		getAll: defineQuery<() => Country[]>(() => useBackendFetch('location/countries')),
 		getCities: defineQuery<(input: { country: Country }) => City[]>((input) => {
 			return useBackendFetch(`location/cities/${input.country}`);
+		}),
+		localize: defineMutation<(input: { country: string }) => string>((input) => {
+			return useBackendFetch(`location/city/localized/${input.country}`);
+		})
+	},
+	city: {
+		localize: defineMutation<(input: { city: string }) => string>((input) => {
+			return useBackendFetch(`location/city/localized/${input.city}`);
 		})
 	}
 };

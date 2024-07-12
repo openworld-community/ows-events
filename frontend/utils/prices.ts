8be8a95d17c12d67a0f6bdbbb65bbd7d1dc22302
Sinamon-maker <nikaterina@gmail.com>
2024-07-12 19:10:35 +0300

    added for every currency number of decimals

--- frontend/utils/prices.ts
+++ frontend/utils/prices.ts
@@ -1,7 +1,9 @@
+import type { Currency } from '~/stores/location.store';
 import type { EventPrice } from '../../common/types/event';
+import { NUMBER_DECIMALS_BY_CURRENCY } from '~/constants/defaultValues/validation';
 
 // Получает аббривеатуру валюты (если существует)
-export const formatCurrency = (currency: string) => {
+export const formatCurrency = (currency: Currency) => {
 	try {
 		const formatter = new Intl.NumberFormat('ru', {
 			style: 'currency',
@@ -23,22 +25,42 @@ export const formatCurrency = (currency: string) => {
 export const formatPrice = (price: EventPrice) => {
 	const { t } = useI18n();
 	// если фиксированная цена, возвращает price currency:
-	if (price?.value && price?.currency) return `${price.value} ${formatCurrency(price.currency)}`;
+	if (price?.value && price?.currency)
+		return `${convertPriceToString(price.value, price.currency)} ${formatCurrency(
+			price.currency
+		)}`;
 	// если интервал, возвращает min-max currency:
 	if (price?.minValue && price?.maxValue && price?.currency)
-		return `${price.minValue}-${price.maxValue} ${formatCurrency(price.currency)}`;
+		return `${convertPriceToString(price.minValue, price.currency)}-${convertPriceToString(
+			price.maxValue,
+			price.currency
+		)} ${formatCurrency(price.currency)}`;
 	// если интервал от, возвращает "От price currency"
 	if (price?.minValue && price?.currency)
-		return `${t('event.price.from')} ${price.minValue} ${formatCurrency(price.currency)}`;
+		return `${t('event.price.from')} ${convertPriceToString(
+			price.minValue,
+			price.currency
+		)} ${formatCurrency(price.currency)}`;
 	// если интервал до, возвращает "До price currency"
 	if (price?.maxValue && price?.currency)
-		return `${t('event.price.to')} ${price.maxValue} ${formatCurrency(price.currency)}`;
+		return `${t('event.price.to')} ${convertPriceToString(
+			price.maxValue,
+			price.currency
+		)} ${formatCurrency(price.currency)}`;
 };
 
 //TODO: выбирает валюту в зависимости от страны (пока валюты прибиты на фронте)
 export const getCurrencyByCountry = (country: string) => {
 	if (country === 'Montenegro') return 'EUR';
 	if (country === 'Serbia') return 'RSD';
-	if (country === 'Kyrgyzstan') return 'KGS';
+
 	return 'USD';
+};
+
+export const getLengthDecimalByCurrency = (currency: Currency) => {
+	return NUMBER_DECIMALS_BY_CURRENCY[currency];
+};
+
+export const convertPriceToString = (price: number, currency: Currency) => {
+	return price.toFixed(getLengthDecimalByCurrency(currency));
 };

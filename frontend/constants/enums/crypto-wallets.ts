78a35d63fd227c5da292233063bab1ccfd02e3cd
Nikita S <nik@mail.ru>
2024-08-08 23:37:49 +0500

    тип сети для USDT и USDC

--- frontend/constants/enums/crypto-wallets.ts
+++ frontend/constants/enums/crypto-wallets.ts
@@ -1,6 +1,6 @@
 export enum CryptoWalletEnum {
 	BITCOIN = 'bc1q0krv6zupalsuz4c8hpqg2e9z6m3klvxxdhwt88',
 	ETHEREUM = '0x4Dac7f4fAC521F170079eA912706BBe731F960a7',
-	USDT = 'TJw5KcaAdbPFnC3EEjLttwaVfEQxBuzWQ3',
-	USDC = 'TJw5KcaAdbPFnC3EEjLttwaVfEQxBuzWQ3'
+	USDT = 'TJw5KcaAdbPFnC3EEjLttwaVfEQxBuzWQ3', 	// Тип сети TRC20
+	USDC = 'TJw5KcaAdbPFnC3EEjLttwaVfEQxBuzWQ3'		// Тип сети TRC20
 }

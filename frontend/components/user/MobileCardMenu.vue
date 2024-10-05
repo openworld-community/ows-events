7ab364baa624a2ddd940a2aee5d4d79390934cbb
Sinamon-maker <nikaterina@gmail.com>
2024-10-05 11:08:18 +0200

    try preventDefault

--- frontend/components/user/MobileCardMenu.vue
+++ frontend/components/user/MobileCardMenu.vue
@@ -11,56 +11,40 @@ defineProps({
 		default: false
 	}
 });
-const test = (event) => {
-	event.preventDefault();
-	event.stopPropogation();
-};
 </script>
 <template>
-	<div style="z-index: 2">
-		<LibraryPopover
-			variant="custom"
-			align="end"
-		>
-			<template #trigger>
-				<button @click="test($event)">
-					<CommonIcon name="dots-three-vertical" />
-				</button>
-			</template>
-			<template #content>
+	<div style="z-index: 2; padding: 10px">
+		<UserUiWrapperMenu>
+			<CommonButton
+				icon-name="edit"
+				button-kind="text"
+				:button-text="$t('user.my_events.buttons.edit')"
+				no-border
+				:class="[
+					'user-buttons__button--edit',
+					{ 'user-buttons__button--edit--expired': isExpired }
+				]"
+				@click="$emit('edit', id)"
+			/>
+			<LibraryAlert
+				:title="$t('modal.delete_event_modal.title')"
+				:description-text="$t('modal.delete_event_modal.text')"
+				confirm-button-text="global.button.confirm"
+				@on-confirm="$emit('delete', id)"
+			>
 				<CommonButton
-					:link="RoutePathEnum.USER_MY_EVENTS"
-					icon-name="edit"
-					button-kind="text"
-					:button-text="$t('user.my_events.buttons.edit')"
+					icon-name="trash"
+					:button-text="$t('user.my_events.buttons.delete')"
+					button-kind="warning"
 					no-border
 					:class="[
-						'user-buttons__button--edit',
-						{ 'user-buttons__button--edit--expired': isExpired }
+						'user-buttons__button--delete',
+						{
+							'user-buttons__button--delete--expired': isExpired
+						}
 					]"
-					@click="$emit('edit', id)"
 				/>
-				<LibraryAlert
-					:title="$t('modal.delete_event_modal.title')"
-					:description-text="$t('modal.delete_event_modal.text')"
-					confirm-button-text="global.button.confirm"
-					@on-confirm="$emit('delete', id)"
-				>
-					<CommonButton
-						:link="RoutePathEnum.USER_MY_EVENTS"
-						icon-name="trash"
-						:button-text="$t('user.my_events.buttons.delete')"
-						button-kind="warning"
-						no-border
-						:class="[
-							'user-buttons__button--delete',
-							{
-								'user-buttons__button--delete--expired': isExpired
-							}
-						]"
-					/>
-				</LibraryAlert>
-			</template>
-		</LibraryPopover>
+			</LibraryAlert>
+		</UserUiWrapperMenu>
 	</div>
 </template>
